8b62e3c26f9611d59234dda4d73a66c4420cfcc2
Derek <dfried10@gmail.com>
2013-02-08 09:13:26 -0500

    Finally fixed URL validation.

--- Podcast_Listener/src/edu/android/podcast_listener/FindCastsActivity.java
+++ Podcast_Listener/src/edu/android/podcast_listener/FindCastsActivity.java
@@ -1,17 +1,28 @@
 package edu.android.podcast_listener;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import edu.android.podcast_listener.util.PodcastConstants;
 
 public class FindCastsActivity extends Activity {
 	private EditText urlInput;
+	Button button;
+	ValidateUrlTask urlTask;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,16 @@ public class FindCastsActivity extends Activity {
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		urlInput = (EditText) findViewById(R.id.editText1);
+		
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				urlTask = new ValidateUrlTask(FindCastsActivity.this);
+				urlTask.execute();
+			}
+		});
+		
 	}
 
 	@Override
@@ -51,5 +72,67 @@ public class FindCastsActivity extends Activity {
 		Intent intent = new Intent(this, FindCastsResultsActivity.class);			
 		intent.putExtra(PodcastConstants.EXTRA_MESSAGE, podcastUrl);
 		startActivity(intent);
+	}
+	
+	public String getEntredURL() {
+		return urlInput.getText().toString();
+	}
+	
+	static class ValidateUrlTask extends AsyncTask<Void, Void, Boolean> {
+		FindCastsActivity activity;
+		ProgressDialog progressDialog;
+		
+		ValidateUrlTask(FindCastsActivity activity) {
+			attach(activity);
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			progressDialog = new ProgressDialog(activity.getApplicationContext());
+			progressDialog = ProgressDialog.show(activity,"","Validating URL.", true,false);
+			super.onPreExecute();
+		}
+		
+		@Override
+		protected Boolean doInBackground(Void... urls) {
+			// TODO Auto-generated method stub
+			boolean isHttpOk = false;
+			HttpURLConnection httpConn = null;
+			
+			try {
+				URL url = new URL(activity.getEntredURL());
+				URLConnection urlConn = url.openConnection();
+				urlConn.setConnectTimeout(5000);
+				httpConn = (HttpURLConnection) urlConn;				
+			    int responseCode = httpConn.getResponseCode();
+			    if (responseCode == HttpURLConnection.HTTP_OK) {
+			    	isHttpOk = true;
+			    }
+				return isHttpOk;
+			} catch (Exception e) {
+				Log.e(PodcastConstants.ERROR_TAG, "Wish I knew why this exception handling isn't working: " + e.getMessage());
+			} finally {
+				if (httpConn != null) {
+					httpConn.disconnect();
+				}
+					
+			}
+			return isHttpOk;
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean valid) {
+			progressDialog.dismiss();
+			if (valid) {
+				activity.toResults(activity.urlInput);
+			} else {
+				Log.e(PodcastConstants.DEBUG_TAG, "Can't validate; oh well.");
+			}
+			super.onPostExecute(valid);
+		}
+		
+		void attach(FindCastsActivity activity) {
+		      this.activity=activity;
+		    }
 	}
 }
